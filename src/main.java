fb33f0f7c6102271a6bc3f004451e6baf939aa0f
Umarov Parviz <parvizumarov78@gmail.com>
2023-01-16 16:50:48 +0500

    added text

--- src/main.java
+++ src/main.java
@@ -4,5 +4,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println(new Date());
+        System.out.println("Хорошего дня!!");
     }
 }
