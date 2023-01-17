3cc18a7754f20698e50ce8b3a58a7851ba703da2
Umarov Parviz <parvizumarov78@gmail.com>
2023-01-17 14:41:39 +0500

    delated last text

--- src/main.java
+++ src/main.java
@@ -4,7 +4,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println(new Date());
-        System.out.println("Goodbye!!");
 
     }
 }
