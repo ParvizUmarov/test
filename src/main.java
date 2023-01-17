2dfe0f447ce59f45009d9347416edb11376d9bb8
Umarov Parviz <parvizumarov78@gmail.com>
2023-01-17 23:40:49 +0500

    delated last text for command stash

--- src/main.java
+++ src/main.java
@@ -4,7 +4,11 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println(new Date());
+<<<<<<< Updated upstream
         System.out.println("Goodbye!!");
+=======
+
+>>>>>>> Stashed changes
 
     }
 }
