f552b08df78024b86c74b98ddef45578b9e23e31
ParvizUmarov <88889472+ParvizUmarov@users.noreply.github.com>
2023-01-17 13:35:28 +0500

    Update main.java

--- src/main.java
+++ src/main.java
@@ -4,6 +4,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println(new Date());
-        System.out.println("Хорошего дня!!");
+        System.out.println("Goodbye!!");
     }
 }
