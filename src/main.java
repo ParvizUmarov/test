cd5f4c62a74184269eb95435db8c8134427e2709
Umarov Parviz <parvizumarov78@gmail.com>
2023-01-17 23:46:32 +0500

    some changes

--- src/main.java
+++ src/main.java
@@ -2,17 +2,10 @@ import java.util.Date;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        String a = "Hello world";
+        System.out.println(a);
         System.out.println(new Date());
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         System.out.println("Goodbye!!");
-=======
-
->>>>>>> Stashed changes
-=======
-
->>>>>>> Stashed changes
 
     }
 }
